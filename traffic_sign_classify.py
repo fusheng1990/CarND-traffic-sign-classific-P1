c438b63e293e3481ec1681e183379c40d874a08c
fusheng1990 <fusheng1990@gmail.com>
2017-08-26 14:05:55 +0800

    Create traffic_sign_classify.py

---
+++ traffic_sign_classify.py
@@ -0,0 +1,544 @@
+###########################Step 0: Load The Data###########################################
+# Load modules
+import cv2
+import csv
+import time
+import glob
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+
+# TODO: Fill this in based on where you saved the training and testing data
+
+training_file = './train.p'
+validation_file='./valid.p'
+testing_file = './test.p'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+    
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+
+### Replace each question mark with the appropriate value.
+################################################### Step 1: Dataset Summary & Exploration####################################
+# TODO: Number of training examples
+n_train = len(y_train)
+
+# TODO: Number of testing examples.
+n_test = len(y_test)
+
+# TODO: What's the shape of an traffic sign image?
+image_shape = X_train.shape
+
+# TODO: How many unique classes/labels there are in the dataset.
+n_classes = len(np.unique(y_train))
+
+img_size=X_train.shape[1] #Size of input images
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+
+### Data exploration visualization goes here.
+### Feel free to use as many code cells as needed.
+# Visualizations will be shown in the notebook.
+%matplotlib inline
+
+num_of_samples=[]
+plt.figure(figsize=(12, 16.5))
+for i in range(0, n_classes):
+    plt.subplot(11, 4, i+1)
+    x_selected = X_train[y_train == i]
+    plt.imshow(x_selected[0, :, :, :]) #draw the first image of each class
+    plt.title(i)
+    plt.axis('off')
+    num_of_samples.append(len(x_selected))
+plt.show()
+
+#Plot number of images per class of train set
+plt.figure(figsize=(12, 4))
+plt.bar(range(0, n_classes), num_of_samples)
+plt.title("Distribution of the train dataset")
+plt.xlabel("Class number")
+plt.ylabel("Number of images")
+plt.show()
+
+print("Min number of images per class =", min(num_of_samples))
+print("Max number of images per class =", max(num_of_samples))
+
+
+######################################## Preprocess the data here.######################################################
+### Feel free to use as many code cells as needed.
+
+# Convert to grayscale
+X_train_rgb = X_train
+X_train_gry = np.sum(X_train/3, axis=3, keepdims=True)
+
+X_test_rgb = X_test
+X_test_gry = np.sum(X_test/3, axis=3, keepdims=True)
+
+X_valid_rgb = X_valid
+X_valid_gry = np.sum(X_valid/3, axis=3, keepdims=True)
+print('RGB shape:', X_train_rgb.shape)
+print('Grayscale shape:', X_train_gry.shape)
+
+# Visualize rgb vs grayscale
+n_rows = 2
+n_cols = 2
+offset = 9000
+fig, axs = plt.subplots(n_rows,n_cols, figsize=(18, 14))
+fig.subplots_adjust(hspace = .1, wspace=.001)
+axs = axs.ravel()
+for j in range(0,n_rows,2):
+    for i in range(n_cols):
+        index = i + j*n_cols
+        image = X_train_rgb[index + offset]
+        axs[index].axis('off')
+        axs[index].imshow(image)
+    for i in range(n_cols):
+        index = i + j*n_cols + n_cols 
+        image = X_train_gry[index + offset - n_cols].squeeze()
+        axs[index].axis('off')
+        axs[index].imshow(image, cmap='gray')
+        
+ #################################### Normalize the train and test datasets #######################################
+
+X_train_normalized = (X_train_gry - 128)/128 
+X_test_normalized = (X_test_gry - 128)/128
+X_valid_normalized = (X_valid_gry - 128)/128
+print(np.mean(X_train_normalized))
+print(np.mean(X_test_normalized))
+
+print("Original shape:", X_train.shape)
+print("Normalized shape:", X_train_normalized.shape)
+fig, axs = plt.subplots(1,2, figsize=(10, 3))
+axs = axs.ravel()
+
+axs[0].axis('off')
+axs[0].set_title('normalized')
+axs[0].imshow(X_train_normalized[0].squeeze(), cmap='gray')
+
+axs[1].axis('off')
+axs[1].set_title('original')
+axs[1].imshow(X_train[0].squeeze(), cmap='gray')
+
+
+
+def random_rotate_img(img):
+    c_x,c_y = int(img.shape[0]/2), int(img.shape[1]/2)
+    ang = 30.0*np.random.rand()-15
+    Mat = cv2.getRotationMatrix2D((c_x, c_y), ang, 1.0)
+    return cv2.warpAffine(img, Mat, img.shape[:2])
+
+def random_scale_img(img):
+    img2=img.copy()
+    sc_y=0.4*np.random.rand()+1.0
+    img2=cv2.resize(img, None, fx=1, fy=sc_y, interpolation = cv2.INTER_CUBIC)
+    
+    dy = int((img2.shape[1]-img.shape[0])/2)
+    end = img.shape[1]-dy
+    img2 = img2[dy:end,:]
+    assert img2.shape[0] == 32
+#     print(img2.shape,dy,end)
+    return img2
+
+def contr_img(img, s=1.0):
+    m=127.0*(1.0-s)
+    return lin_img(img, s, m)
+
+def lin_img(img,s=1.0,m=0.0):
+    img2=cv2.multiply(img, np.array([s]))
+    return cv2.add(img2, np.array([m]))
+
+def scale_img(img):
+    img2=img.copy()
+    sc_y=0.4*np.random.rand()+1.0
+    img2=cv2.resize(img, None, fx=1, fy=sc_y, interpolation = cv2.INTER_CUBIC)
+    c_x,c_y, sh = int(img2.shape[0]/2), int(img2.shape[1]/2), int(img_size/2)
+    return img2
+
+def crop(img, mar=0):
+    c_x,c_y, sh = int(img.shape[0]/2), int(img.shape[1]/2), int(img_size/2-mar)
+    return img[(c_x-sh):(c_x+sh),(c_y-sh):(c_y+sh)]
+
+def augment_img(img):
+    img = img.copy()
+    img = contr_img(img)
+    img = random_rotate_img(img)
+    img = contr_img(img, 1.8*np.random.rand()+0.2)
+   # return scale_img(img)
+    return random_scale_img(img)
+    
+    
+    
+    
+img = X_train[1000]
+plt.subplot(1,2,1)
+plt.imshow(img)
+plt.title('Original Image')
+plt.axis('off')
+plt.subplot(1,2,2)
+plt.axis('off')
+img2 = X_train_normalized[1000]
+out = augment_img(img2[:,:,0]) 
+print(out.shape)
+plt.imshow(out, cmap='gray')
+plt.title('Augmented Image')
+plt.tight_layout()
+plt.savefig('plots/augmented_img_example.png',bbox_inches='tight')
+    
+
+
+####################################### Transform all images and augment training data###############################
+X_train_transf = list()
+y_train_transf = list()
+X_test_transf = list()
+X_valid_transf = list()
+for ii in range(X_train.shape[0]):
+    img = X_train[ii]
+    label = y_train[ii]
+    
+    imgout = X_train_normalized[ii][:,:,0]
+    imgout.shape = (1,) + imgout.shape + (1,)
+    X_train_transf.append(imgout)
+    y_train_transf.append(label)
+    for j in range(5): 
+        imgout = augment_img(X_train_normalized[ii][:,:,0])
+        imgout.shape = (1,) + imgout.shape + (1,)
+        X_train_transf.append(imgout)
+        y_train_transf.append(label)
+
+for ii in range(X_valid.shape[0]):
+    img = X_valid_normalized[ii][:,:,0]
+    img.shape = (1,) + img.shape + (1,)
+    X_valid_transf.append(img)
+    
+for ii in range(X_test.shape[0]):
+    img = X_test_normalized[ii][:,:,0]
+    img.shape = (1,) + img.shape + (1,)
+    X_test_transf.append(img)    
+
+X_valid_transf = np.concatenate(X_valid_transf,axis=0)
+X_test_transf = np.concatenate(X_test_transf,axis=0)
+X_train_transf = np.concatenate(X_train_transf,axis=0)
+y_train_transf = np.array(y_train_transf)
+############################ Step 2: Design and Test a Model Architecture########################################
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+#the reason to add the layer deeper:
+#1. parameter efficiency get much more performance with pure parameters by going deeper rather than wider.
+#2. tend to have a hierarchical structure
+
+########################################Train, Validate and Test the Model########################################
+
+def LeNet(x):
+    """
+    Implement classic lenet architecture in tensorflow
+    """
+    mu = 0
+    sigma = 0.1
+    
+    # Layer 1: Convolutional. Input = 32x32x3. Output = 28x28x6.
+    #variables to allow weights and biases to be modified
+    #the weights must be the dimensions of features by labels. The number of features is the size of the image
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    #  Activation.
+    #ReLU activation function is non-linear, proven to be a universal function approximator
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    #  Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    #  Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # SFlatten. Input = 5x5x16. Output = 400.
+    fc0   = flatten(conv2)
+    
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    #  Activation.
+    fc1    = tf.nn.relu(fc1)
+    
+    dr1 = tf.nn.dropout(fc1, keep_prob) 
+    
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(84))
+    fc2    = tf.matmul(dr1, fc2_W) + fc2_b
+    
+    # Activation.
+    fc2    = tf.nn.relu(fc2)
+    
+    
+    dr2 = tf.nn.dropout(fc2, keep_prob) 
+    
+    #  Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(43))
+    #outputlayer with ReLU activation
+    logits = tf.matmul(dr2, fc3_W) + fc3_b #xw+b
+    
+    return logits
+    
+    
+    # Set the features and labels tensors since they are the inputs to the model
+#Any math operations must have the same type on both sides of the operator. the weights are float32, so the features
+#and labels must also be float32
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+keep_prob = tf.placeholder(tf.float32)
+#to one-hot encode the labels
+one_hot_y = tf.one_hot(y, 43)
+
+#lower the learning rate and get to a better model faster
+#tells the tensorflow how quickly to update the network's weight
+rate = 0.0001
+
+logits = LeNet(x)
+#softmax is convert the logits to pobability one-hot make the highest probability to 1
+#cross entropy sum(one_hot*log(logits))
+#input -> (linear model) -> logits ->softmax -> (cross entropy)-> 1-hot labels
+#to compare those logits to the ground truth labels and calculate the cross entropy
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits,labels= one_hot_y)
+#define loss and optimizer
+#reduce mean function averages the cross entropy from all of the training images.
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+#uses backpropagation to update the network and minimize our training loss
+training_operation = optimizer.minimize(loss_operation)
+predict_operation = tf.argmax(logits, 1)
+predict_proba_operation = tf.nn.softmax(logits=logits)
+
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+#overall accuracy by averaging the individual predictio naccuracies
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def evaluate(X_data, y_data):
+    #set the initial variables
+    num_examples = len(X_data)
+    total_accuracy = 0
+    #batches the dataset and runs it through the evaluation pipline
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+def predict(X_data):
+    num_examples = len(X_data)
+    sess = tf.get_default_session()
+    predicted_proba = list()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x = X_data[offset:offset+BATCH_SIZE]
+        predicted_proba.extend( sess.run(predict_proba_operation, feed_dict={x: batch_x, keep_prob: 1.0}))
+        
+        
+    return predicted_proba
+    
+    
+    
+X_train = X_train_transf
+X_valid = X_valid_transf
+X_test = X_test_transf
+y_train = y_train_transf
+
+from sklearn.utils import shuffle
+from time import time
+#Do some logging
+import logging, datetime
+logger = logging.getLogger()
+
+def setup_file_logger(log_file):
+    hdlr = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr) 
+    logger.setLevel(logging.INFO)
+
+def log(message):
+    #outputs to Jupyter console
+    print('{} {}'.format(datetime.datetime.now(), message))
+    #outputs to file
+    logger.info(message)
+
+setup_file_logger('training.log')
+#An epoch is a single forward and backward pass of the whole dataset. 
+#This is used to increase the accuracy of the model without requiring more data. 
+EPOCHS = 150
+BATCH_SIZE = 128
+#dropout is a regularization technique for reducing overfitting. the technique temporarily drops units from the networks
+#During training, a good starting value for keep_prob is 0.5.
+dropout = .3
+
+errors = list()
+
+saver = tf.train.Saver()
+start = time()
+#create the tensorflow session and initialize the variables
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    log("Training... dropout = {} , batch_size = {} , learning rate = {}".format(dropout, BATCH_SIZE, rate))
+    print()
+    for i in range(EPOCHS):
+        
+        try:
+            #shuffle training data to ensure that our training isn't biased by the order of the images
+            X_train, y_train = shuffle(X_train, y_train)
+#             print("Before Train %d sec"%(time() - start))
+            #break training data into batches and train the model on each batch
+            for offset in range(0, num_examples, BATCH_SIZE):
+                end = offset + BATCH_SIZE
+                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+                #keep_prob to keep units
+                sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1 - dropout})
+            
+#             print("After Train %d sec"%(time() - start))
+            #evaluate the model on our validation data
+            validation_accuracy = evaluate(X_valid, y_valid)
+            training_accuracy = evaluate(X_train, y_train)
+            
+            errors.append((training_accuracy,validation_accuracy))
+            log("EPOCH %d - %d sec ..."%(i+1, time() - start))
+            log("Training error = {:.3f} Validation error = {:.3f}".format(1- training_accuracy , 
+                                                                                   1- validation_accuracy))
+            
+            print()
+            
+#             print("After error computation %d sec"%(time() - start))
+            if i > 5 and i % 3 == 0:
+                saver.save(sess, './models/lenet')
+                print("Model saved %d sec"%(time() - start))
+        except KeyboardInterrupt:
+            print('Accuracy Model On Test Images: {}'.format(evaluate(X_test,y_test)))   
+            break
+            
+    saver.save(sess, './models/lenet')
+#################################Train, Validate and Test the Model###############################################
+plt.figure(figsize=(8,6))
+plt.title('Learning Curve')
+plt.plot([1 - el[0] for el in errors])
+plt.plot([1 - el[1] for el in errors])
+plt.ylim([-.01,0.35])
+plt.legend(['Training Error','Validation Error'])
+plt.tight_layout()
+plt.savefig('plots/learning_curve.png')
+plt.ylabel('Error')
+plt.xlabel('Epoch');
+
+with tf.Session() as sess:
+    #Load the weights and bias
+    saver.restore(sess, tf.train.latest_checkpoint('./models'))
+    print('Accuracy Model On Training Images: {:.2f}'.format(evaluate(X_train,y_train)))
+    print('Accuracy Model On Validation Images: {:.2f}'.format(evaluate(X_valid,y_valid))) 
+    print('Accuracy Model On Test Images: {:.2f}'.format(evaluate(X_test,y_test)))
+    
+    import skimage
+from skimage import io
+from skimage import transform
+from skimage.filters import gaussian
+import glob
+
+i=1
+X_new = []
+plt.figure(figsize=(5,10))
+for el in glob.glob('./images_signs/*.png') + glob.glob('./images_signs/*.jpg'):
+    print(el)
+    img = io.imread(el)    
+#     img = transform.resize(img,(32,32), order=3)
+#     img = gaussian(img,.4,multichannel=True)
+    
+    plt.subplot(1,6,i)
+    plt.imshow(img)
+    i+=1
+    plt.axis('off')
+    
+    img = transform.resize(img,(32,32), order=3)
+    img = gaussian(img,.4,multichannel=True)
+    X_new.append(img)
+    
+plt.savefig('plots/new_images.png',bbox_inches='tight')
+#plt.imshow(X_new[0])
+
+
+###
+print('Processing Image from RGB color to grayscale, resize to 32x32, save into tensor friendly array format... ')
+
+def pre_process_image(image):
+    image = np.uint8(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    return image
+
+img_resize = 32
+N_classes = 43
+image_shape = (img_resize,img_resize)
+img_size_flat = img_resize*img_resize
+
+X_new = np.array([pre_process_image(X_new[i]) for i in range(len(X_new))],
+                          dtype = np.float32)
+
+X_new = X_new.reshape((-1, 32, 32, 1))
+#plt.imshow(X_new[1])
+print('Processing Done.',X_new.shape)
+
+### Visualize the softmax probabilities here.
+### Feel free to use as many code cells as needed.
+i_k5 = np.random.randint(1,len(X_new))
+def test_net(X_data, sess): 
+    prob = sess.run(tf.nn.softmax(logits), feed_dict={x: X_data, keep_prob: 1.0})    
+    top_5 = tf.nn.top_k(prob, k=5)
+    return sess.run(top_5)
+
+
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('./models'))
+    signs_top_5=test_net(X_new, sess)
+    print(signs_top_5[0][0][-1])
+    print(signs_top_5[0][1][-1])
+    
+    
+    ### Visualize the softmax probabilities here.
+### Feel free to use as many code cells as needed.
+plt.figure(figsize=(16, 21))
+for i in range(5):
+    plt.subplot(5, 2, 2*i+1)
+    plt.imshow(img[i]) 
+    plt.title(i)
+    plt.axis('off')
+    plt.subplot(5, 2, 2*i+2)
+    plt.barh(np.arange(1, 6, 1), signs_top_5.values[i, :])
+    labs=[signs_class[j] for j in signs_top_5.indices[i]]
+    plt.yticks(np.arange(1, 6, 1), labs)
+plt.show()
